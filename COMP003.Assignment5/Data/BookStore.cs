bdce5ddac19ee126d993796de35f9aafce6fe175
SalinasJason <jasonsalinas770@gmail.com>
2025-04-23 20:14:06 -0700

    Created Data folder and added a static class name BookStore with three sample records

---
+++ COMP003.Assignment5/Data/BookStore.cs
@@ -0,0 +1,14 @@
+﻿using COMP003.Assignment5.Models;
+namespace COMP003.Assignment5.Data
+{
+    public static class BookStore
+    {
+        public static List<Book> Books { get; } = new()
+        {
+            new Book { Id = 1, Title = "Of Mice and Men", Author = "John Steinbeck", Price = 14.99m },
+            new Book { Id = 2, Title = "The Hunger Games", Author = "Suzanne Collins", Price = 19.99m },
+            new Book { Id = 3, Title = "The Cat in the Hat", Author = "Dr. Seuss", Price = 7.99m },
+        };
+
+    }
+}
