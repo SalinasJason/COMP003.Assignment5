ce9e85687108e909dc72bd9b26949d6935d64f72
SalinasJason <jasonsalinas770@gmail.com>
2025-04-23 19:47:43 -0700

    Created a models folder and added a class name Book with four properties

---
+++ COMP003.Assignment5/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003.Assignment5.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
