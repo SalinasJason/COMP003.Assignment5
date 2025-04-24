ddf5544d508fc1af1082fc0892e34ff71d66f978
SalinasJason <jasonsalinas770@gmail.com>
2025-04-23 21:53:40 -0700

    Added a GET action method to return all records and a GET action method to return one record or NotFound

---
+++ COMP003.Assignment5/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using COMP003.Assignment5.Data;
+using COMP003.Assignment5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003.Assignment5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(BookStore.Books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = BookStore.Books.FirstOrDefault(p => p.Id == id);
+
+            if (book == null)
+            {
+                return NotFound(null);
+            }
+
+            return Ok(book);
+        }
+    }
+}
+
+
+
